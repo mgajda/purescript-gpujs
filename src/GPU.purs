15601d7964ac9123039333a1d41399ebc8cbc0ca
Max Fiialkovskyi <kphloonafh@gmail.com>
2017-08-01 14:28:12 +0000

    minor cleanup

--- src/GPU.purs
+++ src/GPU.purs
@@ -36,15 +36,3 @@ foreign import makeK2 :: ∀ a b c. Options -> String -> Name -> Name -> a -> b 
 foreign import makeK3 :: ∀ a b c d. Options -> String -> Name -> Name -> Name -> a -> b -> c -> d
 foreign import makeK4 :: ∀ a b c d e. Options -> String -> Name -> Name -> Name -> Name -> a -> b -> c -> d -> e
 foreign import makeK5 :: ∀ a b c d e f. Options -> String -> Name -> Name -> Name -> Name -> Name -> a -> b -> c -> d -> e -> f
-
-class In a
-class Out a
-
-instance numParam :: In Number
-instance ar1Param :: In (Array Number)
-instance ar2Param :: In (Array (Array Number))
-instance ar3Param :: In (Array (Array (Array Number)))
-
-instance ar1Output :: Out (Array Number)
-instance ar2Output :: Out (Array (Array Number))
-instance ar3Output :: Out (Array (Array (Array Number)))
